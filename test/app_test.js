268903dddbfac49f2c7c7337194a9af134701c29
Michael Adair <mjadair91@gmail.com>
2020-03-16 10:47:17 +0000

    changed sandbox

--- test/app_test.js
+++ test/app_test.js
@@ -1,47 +1,99 @@
 const assert = require('assert')
-const Rooster = require('../app')
+// const Rooster = require('../app')
+const Calculate = require('../app')
 
-describe('Rooster', () => {
-  describe('announceDawn', () => {
-    it('returns a rooster call', () => {
 
-      //setup
-      const expected = 'cock-a-doodle-doo!'
-      const actualResponse = Rooster.announceDawn()
-      assert.deepEqual(expected, actualResponse)
-    })
+//TDD Testing=====================================================================
+
+
+describe('Calculate', () => {
+  describe('.factorial', () => {
+
+
+    it(())
   })
-  describe('timeAtDawn', () => {
-    it('returns its argument as a string', () => {
-
-      //setup 
-      const timeExpected = '23'
-      const actualTime = Rooster.timeAtDawn(23)
-
-      assert.strictEqual(timeExpected, actualTime)
-    })
-
-    it('throws an error if passed a number less than 0', () => {
-      assert.throws(
-        () => {
-          Rooster.timeAtDawn(-1)
-        },
-        RangeError
-      )
-
-    })
-
-
-    it('throws an error if passed a number greater than 23', () => {
-      assert.throws(
-        () => {
-          Rooster.timeAtDawn(412)
-        },
-        RangeError
-      )
-
-    })
-
-  })
-
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BASIC TESTING================================================================================
+
+// describe('Rooster', () => {
+//   describe('announceDawn', () => {
+//     it('returns a rooster call', () => {
+
+//       //setup
+//       const expected = 'cock-a-doodle-doo!'
+//       const actualResponse = Rooster.announceDawn()
+//       assert.deepEqual(expected, actualResponse)
+//     })
+//   })
+//   describe('timeAtDawn', () => {
+//     it('returns its argument as a string', () => {
+
+//       //setup 
+//       const timeExpected = '23'
+//       const actualTime = Rooster.timeAtDawn(23)
+
+//       assert.strictEqual(timeExpected, actualTime)
+//     })
+
+//     it('throws an error if passed a number less than 0', () => {
+//       assert.throws(
+//         () => {
+//           Rooster.timeAtDawn(-1)
+//         },
+//         RangeError
+//       )
+
+//     })
+
+
+//     it('throws an error if passed a number greater than 23', () => {
+//       assert.throws(
+//         () => {
+//           Rooster.timeAtDawn(412)
+//         },
+//         RangeError
+//       )
+
+//     })
+
+//   })
+
+// })
